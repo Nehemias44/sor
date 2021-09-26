62c53c1d92b5b737648bd8597e875fb062eaef59
Leandro <leandro.nehemias@gmail.com>
2021-09-26 19:36:27 -0300

    Update

--- miniTps/miniTp2/miniTp2.c
+++ miniTps/miniTp2/miniTp2.c
@@ -30,30 +30,26 @@ static void correrB(struct Equipo *e)
 
 int main()
 {
-	sem_t s1A, s2A, s3A, s1B, s2B, s3B, sll;
-	pthread_t h1A, h2A, h3A, h1B, h2B, h3B;
-	sem_init(&s1A, 0, 1); sem_init(&s2A, 0, 1); sem_init(&s3A, 0, 1);
-	sem_init(&s1B, 0, 0); sem_init(&s2B, 0, 0); sem_init(&s3B, 0, 0);
-	sem_init(&sll, 0, 1);
-
-	struct Equipo eq1 = {1, 0, 0, "A", "B", &s1A, &s1B, &sll};
-	struct Equipo eq2 = {2, 0, 0, "A", "B", &s2A, &s2B, &sll};
-	struct Equipo eq3 = {3, 0, 0, "A", "B", &s3A, &s3B, &sll};
-
-	pthread_create(&h1A, NULL, (void*)correrA, (void*)&eq1);
-	pthread_create(&h1B, NULL, (void*)correrB, (void*)&eq1);
-	pthread_create(&h2A, NULL, (void*)correrA, (void*)&eq2);
-	pthread_create(&h2B, NULL, (void*)correrB, (void*)&eq2);
-	pthread_create(&h3A, NULL, (void*)correrA, (void*)&eq3);
-	pthread_create(&h3B, NULL, (void*)correrB, (void*)&eq3);
+	sem_t sem[7]; pthread_t thread[6];
+	struct Equipo eq1 = {1, 0, 0, "A", "B", &sem[0], &sem[1], &sem[7]};
+	struct Equipo eq2 = {2, 0, 0, "A", "B", &sem[2], &sem[3], &sem[7]};
+	struct Equipo eq3 = {3, 0, 0, "A", "B", &sem[4], &sem[5], &sem[7]};
+	
+	sem_init(&sem[0], 0, 1); pthread_create(&thread[0], NULL, (void*)correrA, (void*)&eq1);
+	sem_init(&sem[1], 0, 0); pthread_create(&thread[1], NULL, (void*)correrB, (void*)&eq1);
+	sem_init(&sem[2], 0, 1); pthread_create(&thread[2], NULL, (void*)correrA, (void*)&eq2);
+	sem_init(&sem[3], 0, 0); pthread_create(&thread[3], NULL, (void*)correrB, (void*)&eq2);
+	sem_init(&sem[4], 0, 1); pthread_create(&thread[4], NULL, (void*)correrA, (void*)&eq3);
+	sem_init(&sem[5], 0, 0); pthread_create(&thread[5], NULL, (void*)correrB, (void*)&eq3);
+	sem_init(&sem[6], 0, 1);	
 	
 	printf("Inicio la carrera.\n");
-	pthread_join(h1A, NULL); pthread_join(h1B, NULL);
-	pthread_join(h2A, NULL); pthread_join(h2B, NULL);
-	pthread_join(h3A, NULL); pthread_join(h3B, NULL);
+	pthread_join(thread[0], NULL); pthread_join(thread[1], NULL);
+	pthread_join(thread[2], NULL); pthread_join(thread[3], NULL);
+	pthread_join(thread[4], NULL); pthread_join(thread[5], NULL);
 	printf("Termino la carrera.\n");
 
-	sem_destroy(&s1A); sem_destroy(&s2A);  sem_destroy(&s3A);
-	sem_destroy(&s1B); sem_destroy(&s2B);  sem_destroy(&s3B);
-	sem_destroy(&sll); pthread_exit(NULL); return 0;
+	sem_destroy(&sem[0]); sem_destroy(&sem[1]);  sem_destroy(&sem[2]);
+	sem_destroy(&sem[3]); sem_destroy(&sem[4]);  sem_destroy(&sem[5]);
+	sem_destroy(&sem[7]); pthread_exit(NULL); return 0;
 }
